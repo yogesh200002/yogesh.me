d4351e9f980afb8099f33f89712542d23c60c850
yogesh200002 <yogeshvishal98@gmail.com>
2024-06-22 15:04:02 +0530

    Moved to astro and created portfolio using svelte and astro

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind(), svelte(), mdx()],
+	vite: {
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+	},
+});
---
+++ src/components/contactFooter.svelte
@@ -0,0 +1,38 @@
+<script>
+	import GithubLogo from "@/assets/logo/github.svg?raw";
+	import SvelteLogo from "@/assets/logo/svelte.svg?raw";
+	import AstroLogo from "@/assets/logo/astro.svg?raw";
+	import { Send, Linkedin } from "lucide-svelte";
+</script>
+
+<footer
+	class="mt-auto w-full flex justify-between p-5 border-t border-t-solid border-t-slate-400">
+	<div class="flex gap-2 items-center">
+		Made with <a
+			href="https://astro.build/"
+			class="*:h-6 *:w-6 *:dark:fill-white *:fill-black"
+			id="astroLogo">
+			{@html AstroLogo}
+		</a>
+		and
+		<a
+			href="https://svelte.dev/"
+			class="*:dark:fill-white *:fill-black"
+			id="svelteLogo">
+			{@html SvelteLogo}
+		</a>
+	</div>
+	<div class="flex gap-4 items-center justify-center">
+		<a href="mailto:yogeshvishal98@gmail.com">
+			<Send />
+		</a>
+		<a href="https://www.linkedin.com/in/yogesh-s-8050971a6/">
+			<Linkedin />
+		</a>
+		<a
+			class="*:dark:fill-white *:fill-black"
+			href="https://github.com/yogesh200002/">
+			{@html GithubLogo}
+		</a>
+	</div>
+</footer>
