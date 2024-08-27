4dd77002d1ee139d75383ac8cec801101ab83917
yogesh200002 <yogeshvishal98@gmail.com>
2024-08-27 17:44:37 +0530

    Remove unused typings

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,16 +2,6 @@
 /// <reference types="astro/client" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
-interface ImportMetaEnv {
-	readonly SPOTIFY_CLIENT_ID: string;
-	readonly SPOTIFY_CLIENT_SECRET: string;
-	readonly SPOTIFY_REFRESH_TOKEN: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
-
 declare namespace App {
 	interface Locals extends Runtime {
 		env: {
