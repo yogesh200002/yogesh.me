2aee2af595ffe1d9974d7b5755ae50fcb04dfa7f
yogesh200002 <yogeshvishal98@gmail.com>
2024-08-27 15:38:42 +0530

    Fix cloudflare variables typing and change reference to use cf variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
@@ -13,8 +14,10 @@ interface ImportMeta {
 
 declare namespace App {
 	interface Locals extends Runtime {
-		readonly SPOTIFY_CLIENT_ID: string;
-		readonly SPOTIFY_CLIENT_SECRET: string;
-		readonly SPOTIFY_REFRESH_TOKEN: string;
+		env: {
+			readonly SPOTIFY_CLIENT_ID: string;
+			readonly SPOTIFY_CLIENT_SECRET: string;
+			readonly SPOTIFY_REFRESH_TOKEN: string;
+		};
 	}
 }
