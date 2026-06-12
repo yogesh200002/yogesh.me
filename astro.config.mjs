59dfa1419c470c318e599de08a80a27899333d11
yogesh200002 <yogeshvishal98@gmail.com>
2026-06-12 14:18:16 +0530

    fix: remove Tailwind integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import { fileURLToPath } from "node:url";
-import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), svelte(), mdx()],
+	integrations: [svelte(), mdx()],
 	vite: {
 		resolve: {
 			alias: {
