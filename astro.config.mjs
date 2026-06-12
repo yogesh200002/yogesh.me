46bbf5cf05c4c0337e392a3f27f61fc5c98020a5
Yogesh S <100462257+yogesh200002@users.noreply.github.com>
2026-06-12 15:11:51 +0530

    feat: Upgrade Astro, update configurations, and enhance site content (#65)
    * wip: Astro upgrade
    
    * fix: update personal descriptions and achievements in index.astro and index.mdx
    
    * fix: remove Tailwind integration from astro.config.mjs
    
    * chore: update dependencies and configuration
    
    - Added new Astro integrations: @astrojs/check, @astrojs/cloudflare, @astrojs/mdx, @astrojs/svelte, and @tailwindcss/vite.
    - Updated Astro and Tailwind CSS versions.
    - Removed deprecated Tailwind CSS configuration file.
    - Changed TypeScript configuration to extend from base instead of strict and updated exclude paths.
    
    * fix: remove unused stylesheet link from Layout.astro
    
    * fix: refactor GET function to use getSecret for environment variables
    
    * fix: enhance work experience section with links and formatting improvements
    
    * fix: add Tailwind CSS integration and typography plugin to styles
    
    * fix: remove unused Runtime import from env.d.ts
    
    * fix: add wrangler configuration with compatibility flags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import { fileURLToPath } from "node:url";
-import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), svelte(), mdx()],
+	integrations: [svelte(), mdx()],
 	vite: {
+		plugins: [tailwindcss()],
 		resolve: {
 			alias: {
 				"@": fileURLToPath(new URL("./src", import.meta.url)),
